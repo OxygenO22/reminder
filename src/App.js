11e8793363e472e82722c554c39a11437c37aa63
OxygenO22 <bamaik@mail.ru>
2024-01-10 22:48:25 +0300

    Start memories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if(text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        }
+      ])
+    }
+  }
+
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !== todoId))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label >
+        <input className='addtask' value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+
+      <ul>
+        {
+          todos.map(todo => 
+            <li key={todo.id}>
+              <input type='checkbox' />
+              <span>{todo.text}</span>
+              <span className='delete' onClick={() => removeTodo(todo.id)}>&times;</span>
+            </li>
+          )
+        }
+      </ul>
     </div>
   );
 }
